b961c30069e4fd27038e410756eac7e0b5a7ed9f
Caio Silva <caio.lopes.silva@gmail.com>
2021-03-03 09:20:19 +0100

    bootstrap layout.html implementation
    Additionally, some routes.py testing

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from .config import Development
 
 def create_app(app_config='development'):
 
     app=Flask(__name__)
-    
+    Bootstrap(app)
+
     print(config)
     app.config.from_object(Development())
     from app.main.routes import main
--- app/main/routes.py
+++ app/main/routes.py
@@ -5,3 +5,15 @@ main = Blueprint('main',__name__)
 @main.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@main.route('/about')
+def about():
+    return 'The about page'
+
+@main.route('/blog')
+def blog():
+    return 'This is the blog'
+
+@main.route('/blog/<int:blog_id>')
+def blogpost(blog_id):
+    return 'This is the blogpost number' + str(blog_id)
