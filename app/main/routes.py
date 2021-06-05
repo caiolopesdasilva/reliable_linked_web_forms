57f155f245c4e004d066683bce5730ddce11ac1b
caiolopesdasilva <c.lopesdasilva@student.vu.nl>
2021-06-05 14:46:23 +0200

    Bootstrap forms and onto update

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template,request
-from ..forms import SignUpForm
 main = Blueprint('main',__name__)
 
 @main.route('/', methods=['GET'])
@@ -10,19 +9,10 @@ def index():
 def about():
     return render_template('about.html')
 
-@main.route('/example', methods=['GET','POST'])
+@main.route('/example', methods=['GET', 'POST'])
 def main_form():
-    form = formexample()
-    if form.is_submitted():
-        result = request.form
-        return render_template('user.html',result=result)
     return render_template('example.html')
 
-
-@main.route('/signup', methods=['GET','POST'])
+@main.route('/signup', methods=['GET', 'POST'])
 def signup():
-    form = SignUpForm()
-    if form.is_submitted():
-        result = request.form
-        return render_template('user.html', result=result)
-    return render_template('signup.html', form=form)
+    return render_template('signup.html')
